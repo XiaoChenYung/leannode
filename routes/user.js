bf4ee7efe910c177519478ed2898be384397c4ff
杨晓晨 <xiaochenyung@gmail.com>
2017-06-21 15:00:09 +0800

    添加注册View

---
+++ routes/user.js
@@ -0,0 +1,14 @@
+/**
+ * Created by tm on 2017/6/21.
+ */
+'use strict';
+var router = require('express').Router();
+var AV = require('leanengine');
+var moment = require('moment');
+var User = AV.Object.extend('_User');
+
+router.get('/singup', function (req, res, next) {
+    res.render('singup');
+})
+
+module.exports = router;
